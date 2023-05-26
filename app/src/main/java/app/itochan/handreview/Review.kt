c7c739c58e17c6b69da681ed91827106e166fadd
Kazunori Jo <519604+itochan@users.noreply.github.com>
2023-05-27 07:02:14 +0900

    Show sample hand review

--- app/src/main/java/app/itochan/handreview/Review.kt
+++ app/src/main/java/app/itochan/handreview/Review.kt
@@ -1,21 +1,139 @@
 package app.itochan.handreview
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Button
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import app.itochan.handreview.ui.theme.PokerHandReviewAppTheme
 
 @Composable
 fun Review(navController: NavHostController) {
+  val playerA = "Player A"
+  val playerB = "Player B"
+  val positionBtn = "BTN"
+  val positionBb = "BB"
+
   PokerHandReviewAppTheme {
-    Column(modifier = Modifier.fillMaxSize()) {
-      Button(onClick = { navController.navigate("choose_card") }) {
-        Text(text = "Choose card")
+    Row(
+      modifier = Modifier
+        .fillMaxSize()
+        .padding(8.dp)
+    ) {
+      Column(modifier = Modifier.weight(1f)) {
+        Text(
+          modifier = Modifier.padding(bottom = 8.dp),
+          text = "Preflop"
+        )
+        Action(
+          name = playerA,
+          position = positionBtn,
+          action = "Raise 2"
+        )
+        Action(
+          name = playerB,
+          position = positionBb,
+          action = "Call 1.50"
+        )
+      }
+      Column(modifier = Modifier.weight(1f)) {
+        Text(
+          modifier = Modifier.padding(bottom = 8.dp),
+          text = "Flop"
+        )
+        Action(
+          name = playerA,
+          position = positionBtn,
+          action = "Check"
+        )
+        Action(
+          name = playerB,
+          position = positionBb,
+          action = "Bet 2.50"
+        )
+        Action(
+          name = playerA,
+          position = positionBtn,
+          action = "Call 2.50"
+        )
+      }
+      Column(modifier = Modifier.weight(1f)) {
+        Text(
+          modifier = Modifier.padding(bottom = 8.dp),
+          text = "Turn"
+        )
+        Action(
+          name = playerA,
+          position = positionBtn,
+          action = "Check"
+        )
+        Action(
+          name = playerB,
+          position = positionBb,
+          action = "Bet 5"
+        )
+        Action(
+          name = playerA,
+          position = positionBtn,
+          action = "Raise 15"
+        )
+        Action(
+          name = playerB,
+          position = positionBb,
+          action = "Call 10"
+        )
+      }
+      Column(modifier = Modifier.weight(1f)) {
+        Text(
+          modifier = Modifier.padding(bottom = 8.dp),
+          text = "River"
+        )
+        Action(
+          name = playerA,
+          position = positionBtn,
+          action = "Bet 30"
+        )
+        Action(
+          name = playerB,
+          position = positionBb,
+          action = "Call 30"
+        )
+        Action(
+          name = playerA,
+          position = positionBtn,
+          action = "6d 8d"
+        )
+        Action(
+          name = playerB,
+          position = positionBb,
+          action = "Jd Kd"
+        )
       }
     }
+  }
+}
+
+@Composable
+private fun Action(
+  name: String,
+  position: String,
+  action: String
+) {
+  Column(
+    modifier = Modifier.padding(bottom = 8.dp)
+  ) {
+    Text(
+      text = "$name [$position]",
+      fontSize = 12.sp
+    )
+    Text(
+      text = action,
+      fontSize = 12.sp
+    )
   }
 }
