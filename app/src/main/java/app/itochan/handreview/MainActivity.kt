605db62a02cca28fc215e24aadb2eefab8d300d3
Kazunori Jo <519604+itochan@users.noreply.github.com>
2023-05-27 05:48:56 +0900

    Put choose card button

--- app/src/main/java/app/itochan/handreview/MainActivity.kt
+++ app/src/main/java/app/itochan/handreview/MainActivity.kt
@@ -3,9 +3,9 @@ package app.itochan.handreview
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -17,27 +17,25 @@ class MainActivity : ComponentActivity() {
     super.onCreate(savedInstanceState)
     setContent {
       PokerHandReviewAppTheme {
-        // A surface container using the 'background' color from the theme
-        Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-          Greeting("Android")
-        }
+        HandReviewScreen()
       }
     }
   }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-  Text(
-    text = "Hello $name!",
-    modifier = modifier
-  )
+fun HandReviewScreen() {
+  Column(modifier = Modifier.fillMaxSize()) {
+    Button(onClick = { /*TODO*/ }) {
+      Text(text = "Choose card")
+    }
+  }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun HandReviewPreview() {
   PokerHandReviewAppTheme {
-    Greeting("Android")
+    HandReviewScreen()
   }
 }
